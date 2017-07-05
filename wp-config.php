2f482a8fad8f02c5fb8a5fa0293607142e2291c4
Jourdan Rodrigues <thiagojourdan@gmail.com>
2017-07-05 15:11:32 -0300

    :ambulance: Not quoted string.

--- wp-config.php
+++ wp-config.php
@@ -5,9 +5,9 @@ define('DB_NAME', getenv('DB_NAME'));
 /** MySQL database username */
 define('DB_USER', getenv('DB_USER'));
 /** MySQL database password */
-define('DB_PASSWORD',getenv('DB_PASSWORD'));
+define('DB_PASSWORD', getenv('DB_PASSWORD'));
 /** MySQL hostname */
-define('DB_HOST', getenv('DB_HOST') . : . getenv('DB_PORT'));
+define('DB_HOST', getenv('DB_HOST') . ':' . getenv('DB_PORT'));
 /** Database Charset to use in creating database tables. */
 define('DB_CHARSET', 'utf8');
 /** The Database Collate type. Don't change this if in doubt. */
