76a34a8658c5808effc1272e8069a575274376d8
Jourdan Rodrigues <thiagojourdan@gmail.com>
2017-07-05 15:08:18 -0300

    :ambulance: Missing PHP tag to "wp-config.php".

---
+++ wp-config.php
@@ -0,0 +1,35 @@
+<?php
+// ** MySQL settings - You can get this info from your web host ** //
+/** The name of the database for WordPress */
+define('DB_NAME', getenv('DB_NAME'));
+/** MySQL database username */
+define('DB_USER', getenv('DB_USER'));
+/** MySQL database password */
+define('DB_PASSWORD',getenv('DB_PASSWORD'));
+/** MySQL hostname */
+define('DB_HOST', getenv('DB_HOST') . : . getenv('DB_PORT'));
+/** Database Charset to use in creating database tables. */
+define('DB_CHARSET', 'utf8');
+/** The Database Collate type. Don't change this if in doubt. */
+define('DB_COLLATE', '');
+
+/** Server settings for Jetpack */
+['SERVER_PORT'] = 80;
+
+/**#@+
+* Authentication Unique Keys and Salts.
+*
+* Change these to different unique phrases!
+* You can generate these using the {@link https://api.wordpress.org/secret-key/1.1/salt/ WordPress.org secret-key service}
+* You can change these at any point in time to invalidate all existing cookies. This will force all users to have to log in again.
+*
+* @since 2.6.0
+*/
+define('AUTH_KEY', getenv('AUTH_KEY'));
+define('SECURE_AUTH_KEY', getenv('SECURE_AUTH_KEY'));
+define('LOGGED_IN_KEY', getenv('LOGGED_IN_KEY'));
+define('NONCE_KEY', getenv('NONCE_KEY'));
+define('AUTH_SALT', getenv('AUTH_SALT'));
+define('SECURE_AUTH_SALT', getenv('SECURE_AUTH_SALT'));
+define('LOGGED_IN_SALT', getenv('LOGGED_IN_SALT'));
+define('NONCE_SALT', getenv('NONCE_SALT'));
